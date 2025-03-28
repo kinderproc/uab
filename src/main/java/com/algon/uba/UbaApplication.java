e699741bc043dee645ac7cf2b25fda690ccfc8fe
kinderproc <kinderproc84@yandex.ru>
2025-03-28 19:10:29 +0300

    fix: added missing project files

---
+++ src/main/java/com/algon/uba/UbaApplication.java
@@ -0,0 +1,13 @@
+package com.algon.uba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algon/uba/UbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algon.uba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
